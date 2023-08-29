68fea917274a1e69887ddd39767db496cc24bca9
SebaDL <jsdilauro@gmail.com>
2023-08-29 19:47:16 -0300

    Add UI Button to test (wip)

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,12 @@ import { testMeshes } from './test/mesh'
 import { testTextShape } from './test/text-shape'
 import { testCameraModeArea } from './test/camera-mode'
 import { testRaycast } from './test/raycast'
+import { testUi } from './test/ui-test'
+import { ReactEcsRenderer } from '@dcl/sdk/react-ecs'
+
 
 export function main() {
+
   engine.addSystem(postColliderSystem)
   engine.addSystem(pointerVideoSystem)
   testGltfs(Vector3.create(1, 1, 12))
@@ -17,4 +21,5 @@ export function main() {
   testTextShape(Vector3.create(13, 1, 12))
   testCameraModeArea(Vector3.create(1, 1, 8))
   //testRaycast(Vector3.create(5,1,8))
+  testUi(Vector3.create(9, 1, 8))
 }
