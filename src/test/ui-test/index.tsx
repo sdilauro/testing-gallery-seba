95fb13e2446bfb27f540104a16fb967fb506ebab
SebaDL <jsdilauro@gmail.com>
2023-08-29 19:47:16 -0300

    Add UI Button to test (wip)

--- src/test/ui-test/index.tsx
+++ src/test/ui-test/index.tsx
@@ -1,15 +1,16 @@
-import ReactEcs, { Label, ReactEcsRenderer, UiEntity} from '@dcl/sdk/react-ecs'
+import ReactEcs, { Button, Label, ReactEcsRenderer, UiEntity} from '@dcl/sdk/react-ecs'
 
 import { BackgroundTextureMode, Transform, UiBackground, UiText, engine } from '@dcl/sdk/ecs'
 import { Color4, Vector3 } from '@dcl/sdk/math'
 import { circularSlider } from '../../helper/circular-slider'
 import { createPost } from '../../helper/window-post'
 
-export function testUiText(position: Vector3) {
+export function testUi(position: Vector3) {
   const post = createPost(position, { windowSize: { x: 1.5, y: 1.5 }, fontSize: 1 }).move(Vector3.create(2, 0, 0))
   const UiTextTimer = circularSlider(1.0)
+  const textValue:string = "Welcome:\nThis is a test scene. Make sure the skybox light is 12:00 to see entities as in the screenshots."
 
-
+  let backgroundColor:Color4=Color4.Black()
 
 
   engine.addSystem(UiTextTimer.system)
@@ -20,7 +21,7 @@ export function testUiText(position: Vector3) {
   const uiTextEntity = engine.addEntity()
   Transform.create(uiTextEntity, { position })
 
- 
+  
 
   UiTextTimer.add((index) => {
     ReactEcsRenderer.setUiRenderer(() => (
@@ -31,15 +32,22 @@ export function testUiText(position: Vector3) {
           margin: {
             top: '35px', left: '275px' }
         }}
-        uiBackground={{ color: Color4.Black() }}
+        uiBackground={{ color:backgroundColor }}
       >
         <Label
           uiTransform={{margin:{top:10,left:20}}}
-          value="Welcome:\nThis is a test scene. Make sure the skybox light is 12:00 to see entities as in the screenshots."
+          value={textValue}
           color={Color4.White()}
-          fontSize={25}
+          fontSize={20}
           font="serif"
           textAlign="top-left"
+        />
+        <Button
+          value= {`Hold to change \n background color \n to blue`}
+          variant="primary"
+          uiTransform={{ width: 100, height: 60, margin: { left:1060, top:20 } }}
+          onMouseDown={() => { backgroundColor = Color4.Blue() }}
+          onMouseUp={() => { backgroundColor = Color4.Black() }}
         />
       </UiEntity>
     ))
@@ -60,11 +68,17 @@ export function testUiText(position: Vector3) {
       >
         <Label
           uiTransform={{margin:{top:10,left:20}}}
-          value="Welcome:\nThis is a test scene. Make sure the skybox light is 12:00 to see entities as in the screenshots."
+          value={textValue}
           color={Color4.Red()}
-          fontSize={25}
+          fontSize={20}
           font="serif"
           textAlign="top-left"
+        />
+        <Button
+          value= "Click me"
+          variant="primary"
+          uiTransform={{ width: 100, height: 60, margin: { left:1060, top:20 } }}
+          onMouseDown={() => { console.log("Clicked on the UI") } }
         />
       </UiEntity>
     ))
@@ -86,3 +100,4 @@ export function testUiText(position: Vector3) {
   })
 
 }
+
