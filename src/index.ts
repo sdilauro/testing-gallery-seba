7fb755ce348f08d8ffa032f79b0aaeb7fc3d4daa
SebaDL <jsdilauro@gmail.com>
2023-09-01 01:43:31 -0300

    Add test material b

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,8 @@ import { testMaterial } from './test/material'
 import { testMeshes } from './test/mesh'
 import { testTextShape } from './test/text-shape'
 import { testCameraModeArea } from './test/camera-mode'
-import { testRaycast } from './test/raycast'
 import { testUi } from './test/ui-test'
-import { ReactEcsRenderer } from '@dcl/sdk/react-ecs'
+import { testMaterialB } from './test/material-b'
 
 
 export function main() {
@@ -20,6 +19,6 @@ export function main() {
   testMaterial(Vector3.create(9, 1, 12))
   testTextShape(Vector3.create(13, 1, 12))
   testCameraModeArea(Vector3.create(1, 1, 8))
-  //testRaycast(Vector3.create(5,1,8))
+  testMaterialB(Vector3.create(5, 1, 8))
   testUi(Vector3.create(9, 1, 8))
 }
